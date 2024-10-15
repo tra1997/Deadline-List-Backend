491d17f13c1cdbd47e024f817a9e5ce3d1051194
tra1997 <tratran2019@gmail.com>
2024-10-15 21:42:16 +0700

    final

--- src/main/java/com/example/todo_management/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.todo_management.repository;
-
-import com.example.todo_management.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    
-Role findByName(String name);
-}
